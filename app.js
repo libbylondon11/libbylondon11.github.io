b7063d049c0a8e7d5dc3e8f7eb0e6e05982df1a1
libbylondon <libbylondon22@gmail.com>
2017-04-26 12:29:33 -0500

    lightbox

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ $('a.thumb').click(function(event){
     content.html($(this).html());
     $(".modal-profile").modal({show:true});
 });
+$('.thumbnail').click(function(){
+  	$('.modal-body').empty();
+  	var title = $(this).attr("title");
+  	$('.modal-title').html(title);
+  	$(this.innerHTML).appendTo('.modal-body');
+  	$('#myModal').modal({show:true});
+});
 $(document).ready(function () {
 
 $(function(){
