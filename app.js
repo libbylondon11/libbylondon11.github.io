93a022fd01a3c710e9538f482820d06438f051d6
libbylondon <libbylondon22@gmail.com>
2017-04-26 12:47:01 -0500

    add

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ $('a.thumb').click(function(event){
     content.html($(this).html());
     $(".modal-profile").modal({show:true});
 });
-$('.thumbnail').click(function(){
-  	$('.modal-body').empty();
-  	var title = $(this).attr("title");
-  	$('.modal-title').html(title);
-  	$(this.innerHTML).appendTo('.modal-body');
-  	$('#myModal').modal({show:true});
-});
+
 $(document).ready(function () {
 
 $(function(){
